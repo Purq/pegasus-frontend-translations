0b4327b8873ddf5bbceabff7aa92696663217024
Purq <purg.derren@gmail.com>
2018-10-31 22:06:34 +0100

    Create pegasus_ru.ts

---
+++ pegasus_ru.ts
@@ -0,0 +1,305 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>FilterPanel</name>
+    <message>
+        <location filename="../src/themes/pegasus-grid/filter_panel/FilterPanel.qml" line="53"/>
+        <source>Filters</source>
+        <translation type="фильтр"></translation>
+    </message>
+    <message>
+        <location filename="../src/themes/pegasus-grid/filter_panel/FilterPanel.qml" line="66"/>
+        <source>title</source>
+        <translation type="название"></translation>
+    </message>
+    <message>
+        <location filename="../src/themes/pegasus-grid/filter_panel/FilterPanel.qml" line="81"/>
+        <source>Favorites</source>
+        <translation type="избранное"></translation>
+    </message>
+    <message>
+        <location filename="../src/themes/pegasus-grid/filter_panel/FilterPanel.qml" line="94"/>
+        <source>Multiplayer</source>
+        <translation type="Мультиплеер"></translation>
+    </message>
+</context>
+<context>
+    <name>GamepadScreen</name>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="110"/>
+        <source>No gamepads connected</source>
+        <translation type="Нет подключенных контроллеров"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="168"/>
+        <source>left back</source>
+        <translation type="Сзади слева"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="179"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="346"/>
+        <source>shoulder</source>
+        <translation type="плечное"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="194"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="360"/>
+        <source>trigger</source>
+        <translation type="Курок"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="210"/>
+        <source>dpad</source>
+        <translation type="панель управления"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="219"/>
+        <source>up</source>
+        <translation type="вверх"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="234"/>
+        <source>down</source>
+        <translation type="вниз"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="249"/>
+        <source>left</source>
+        <translation type="влево"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="264"/>
+        <source>right</source>
+        <translation type="вправо"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="280"/>
+        <source>left stick</source>
+        <translation type="Левая ручка"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="290"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="453"/>
+        <source>x axis</source>
+        <translation type="Ось-X"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="305"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="467"/>
+        <source>y axis</source>
+        <translation type="Ось-Y"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="320"/>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="481"/>
+        <source>press</source>
+        <translation type="нажмите"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="335"/>
+        <source>right back</source>
+        <translation type="Задний правый"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="375"/>
+        <source>abxy</source>
+        <translation type="ABXY"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="442"/>
+        <source>right stick</source>
+        <translation type="Правая ручка"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="502"/>
+        <source>center</source>
+        <translation type="центр"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="511"/>
+        <source>select</source>
+        <translation type="выбрать"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="527"/>
+        <source>guide</source>
+        <translation type="гайд"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="542"/>
+        <source>start</source>
+        <translation type="запустить"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/GamepadScreen.qml" line="634"/>
+        <source>hold down to quit</source>
+        <translation type="Нажмите и удерживайте, чтобы закрыть"></translation>
+    </message>
+</context>
+<context>
+    <name>GenericOkCancelDialog</name>
+    <message>
+        <location filename="../src/frontend/dialogs/GenericOkCancelDialog.qml" line="164"/>
+        <source>Ok</source>
+        <translation type="OK"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/dialogs/GenericOkCancelDialog.qml" line="196"/>
+        <source>Cancel</source>
+        <translation type="сбросить"></translation>
+    </message>
+</context>
+<context>
+    <name>MainMenuPanel</name>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="49"/>
+        <source>Settings</source>
+        <translation type="настройки"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="60"/>
+        <source>Controls</source>
+        <translation type="управление"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="71"/>
+        <source>Quit</source>
+        <translation type="выход"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="77"/>
+        <source>Shutdown</source>
+        <translation type="выключить"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="86"/>
+        <source>Reboot</source>
+        <translation type="перезапустить"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/MainMenuPanel.qml" line="93"/>
+        <source>Exit Pegasus</source>
+        <translation type="закрыть Pegasus"></translation>
+    </message>
+</context>
+<context>
+    <name>NoGamesError</name>
+    <message>
+        <location filename="../src/frontend/messages/NoGamesError.qml" line="42"/>
+        <source>No games found :(</source>
+        <translation type="Игры не найдены :("></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/NoGamesError.qml" line="57"/>
+        <source>Pegasus couldn&apos;t find any games on your device. If you have not set up Pegasus yet, you can find the documentation here: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;If you still see this message, make sure your config files are readable, exist in one of the expected locations and are in the expected format.</source>
+        <translation type="Pegasus не смог найти игры на устройстве. Если вы еще не настроили Pegasus, вы можете посмотреть документацию здесь: &lt;i&gt;%1&lt;/i&gt;.&lt;br&gt;Если вы еще видите это сообщение, убедитесь, что файлы конфигурации доступны для чтения и в соответствующем каталоге и в формате."></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/NoGamesError.qml" line="75"/>
+        <source>Please see the log file for more details.</source>
+        <translation type="Для получения дополнительной информации см. Log-Файл."></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/NoGamesError.qml" line="87"/>
+        <source>You can find it here:&lt;pre&gt;%1&lt;/pre&gt;</source>
+        <translation type="Вы можете найти здесь:&lt;pre&gt;%1&lt;/pre&gt;"></translation>
+    </message>
+</context>
+<context>
+    <name>RebootDialog</name>
+    <message>
+        <location filename="../src/frontend/dialogs/RebootDialog.qml" line="25"/>
+        <source>Reboot</source>
+        <translation type="перезапуск"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/dialogs/RebootDialog.qml" line="26"/>
+        <source>The system will reboot. Are you sure?</source>
+        <translation type="Система будет перезагружена, вы уверены?"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsScreen</name>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="49"/>
+        <source>Settings</source>
+        <translation type="настройки"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="89"/>
+        <source>Language</source>
+        <translation type="язык"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="109"/>
+        <source>Theme</source>
+        <translation type="тема"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="129"/>
+        <source>Fullscreen mode</source>
+        <translation type="полноэкранный режим"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/menu/SettingsScreen.qml" line="130"/>
+        <source>On some platforms this setting may have no effect</source>
+        <translation type="В некоторых системах эта настройка может не иметь эффекта."></translation>
+    </message>
+</context>
+<context>
+    <name>ShutdownDialog</name>
+    <message>
+        <location filename="../src/frontend/dialogs/ShutdownDialog.qml" line="25"/>
+        <source>Shutdown</source>
+        <translation type="выключить"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/dialogs/ShutdownDialog.qml" line="26"/>
+        <source>The system will shut down. Are you sure?</source>
+        <translation type="Система выключается, вы уверены?"></translation>
+    </message>
+</context>
+<context>
+    <name>SplashScreen</name>
+    <message>
+        <location filename="../src/frontend/SplashScreen.qml" line="53"/>
+        <source>games
+found</source>
+        <translation type="найдены Игры></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/SplashScreen.qml" line="87"/>
+        <source>looking for games</source>
+        <translation type="поиск игр"></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/SplashScreen.qml" line="113"/>
+        <source>looking for metadata</source>
+        <translation type="поиск метаданных"></translation>
+    </message>
+</context>
+<context>
+    <name>ThemeError</name>
+    <message>
+        <location filename="../src/frontend/messages/ThemeError.qml" line="42"/>
+        <source>Theme loading failed :(</source>
+        <translation type="Ошибка загрузки темы :("></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/ThemeError.qml" line="57"/>
+        <source>Pegasus tried to load the selected theme (%1), but failed. This may happen when you try to load an outdated theme, or when there&apos;s a bug in its code.</source>
+        <translation type="Pegasus попытался загрузить выбранную тему (%1), но не удалось. Это может произойти, если вы попытались загрузить устаревшую тему или если код сломан."></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/ThemeError.qml" line="73"/>
+        <source>For now, please select a different theme from the main menu.</source>
+        <translation type="Пожалуйста, выберите другую тему для главного меню."></translation>
+    </message>
+    <message>
+        <location filename="../src/frontend/messages/ThemeError.qml" line="85"/>
+        <source>You might find more details in the log file:&lt;pre&gt;%1&lt;/pre&gt;</source>
+        <translation type="вы можете найти более подробную информацию в Log-файлах:&lt;pre&gt;%1&lt;/pre&gt;"></translation>
+    </message>
+</context>
+</TS>
